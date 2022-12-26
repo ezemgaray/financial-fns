245881b06a1fac69e09c42ebe9a8ecc4aaa0b0ee
Ezequiel Garay <ezemgaray@gmail.com>
2022-12-26 15:09:38 +0100

    refactor: uninstall dotenv

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,8 @@
 import type { Config } from '@jest/types'
-require('dotenv').config()
 
 const config: Config.InitialOptions = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
-	setupFiles: ['dotenv/config'],
 }
 
 export default config

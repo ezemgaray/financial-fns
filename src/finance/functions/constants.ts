5c9cde0d34760d3c86f7629be76ff60a2ad3e632
Ezequiel Garay <ezemgaray@gmail.com>
2022-12-26 14:48:43 +0100

    chore: Move constants into functions

--- src/finance/functions/constants.ts
+++ src/finance/functions/constants.ts
@@ -1,3 +1,23 @@
+export const FINANCIAL_YEAR_IN_MONTHS = 12
+export const FINANCIAL_YEAN_IN_WEEKS = 52
+export const FINANCIAL_YEAR_IN_DAYS = 360
+export const FINANCIAL_MONTH_IN_DAYS = 30
+export const NATURAL_YEAR_IN_DAYS = 365
+/**
+ * The frequencies can help you calculate how many transactions
+ * there are in a month, year and also define due dates and start of loan installments.
+ */
+export const FREQUENCIES = {
+	weekly: 7,
+	biWeekly: 14,
+	monthly: 1,
+	everyTwoMonths: 2,
+	everyThreeMonths: 3,
+	everyFourMonths: 4,
+	everySixMonths: 6,
+	yearly: 12
+} as const
+
 /**
  * create enum type from object values
  * @example
@@ -15,25 +35,5 @@ export type CashFlowTransaction = {
 }
 export type CashFlow = number[]
 export type CashFlowXirr = CashFlowTransaction[]
-
-export const FINANCIAL_YEAR_IN_MONTHS = 12
-export const FINANCIAL_YEAN_IN_WEEKS = 52
-export const FINANCIAL_YEAR_IN_DAYS = 360
-export const FINANCIAL_MONTH_IN_DAYS = 30
-export const NATURAL_YEAR_IN_DAYS = 365
-/**
- * The frequencies can help you calculate how many transactions
- * there are in a month, year and also define due dates and start of loan installments.
- */
-export const FREQUENCIES = {
-	weekly: 7,
-	biWeekly: 14,
-	monthly: 28,
-	everyTwoMonths: 2,
-	everyThreeMonths: 3,
-	everyFourMonths: 4,
-	everySixMonths: 6,
-	yearly: 12,
-} as const
-
 export type Frequency = ValueOf<typeof FREQUENCIES>
+export type LastDateOfMonth = 28 | 29 | 30 | 31
